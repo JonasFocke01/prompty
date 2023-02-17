bd9b6cf18009e592795ef67182713ca0e8371dbe
Jonas <jonas-focke@mailbox.org>
2023-02-17 08:18:20 +0100

    feat: ➖ remove unused TimestampTypes

--- src/main.rs
+++ src/main.rs
@@ -8,29 +8,24 @@ use chrono::{format, Local, NaiveTime};
 const MIN_WAKEUP_TIME: &str = "6:30";
 const MAX_WAKEUP_TIME: &str = "8:22";
 const SUNRISE_MODIFIER_FOR_WAKE_UP_TIME_IN_MINUTES: i64 = 15;
-const OPTIMAL_EVENING_DINNER_TIME_SINCE_SUNRISE_IN_HOURS: f32 = 11.5;
-const OPTIMAL_SPORTS_TIME_SINCE_SUNRISE_IN_HOURS: i64 = 11;
+const DINNER_TIME_SINCE_SUNRISE_IN_HOURS: f32 = 11.5;
 const SUNRISE_MODIFIER_FOR_BED_TIME_IN_HOURS: f32 = 15.5;
 
 #[derive(PartialEq)]
 enum TimestampType {
     WakeUpTime(NaiveTime),
-    OptimalSportTime(NaiveTime),
     BedTime(NaiveTime),
-    OptimalEveningDinnerTime(NaiveTime),
-    Sunrise(NaiveTime),
+    DinnerTime(NaiveTime),
 }
 
 impl std::fmt::Debug for TimestampType {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             TimestampType::WakeUpTime(_) => write!(f, "Wake up time"),
-            TimestampType::OptimalSportTime(_) => write!(f, "Optimal sports time"),
-            TimestampType::OptimalEveningDinnerTime(_) => {
+            TimestampType::DinnerTime(_) => {
                 write!(f, "Optimal evening dinner time")
             }
             TimestampType::BedTime(_) => write!(f, "Bedtime"),
-            TimestampType::Sunrise(_) => write!(f, "Sunrise"),
         }
     }
 }
@@ -39,27 +34,22 @@ impl TimestampType {
     fn get_naive_time(&self) -> NaiveTime {
         match self {
             TimestampType::WakeUpTime(v)
-            | TimestampType::OptimalSportTime(v)
             | TimestampType::BedTime(v)
-            | TimestampType::OptimalEveningDinnerTime(v)
-            | TimestampType::Sunrise(v) => *v,
+            | TimestampType::DinnerTime(v) => *v,
         }
     }
 }
 
 struct Timestamps {
     wake_up_time: TimestampType,
-    optimal_sport_time: TimestampType,
     bed_time: TimestampType,
-    optimal_evening_dinner_time: TimestampType,
-    _sunrise: TimestampType,
+    dinner_time: TimestampType,
 }
 
 impl Timestamps {
     fn new() -> Timestamps {
         let sunrise = gather_input();
         Timestamps {
-            sunrise: TimestampType::Sunrise(sunrise),
             wake_up_time: TimestampType::WakeUpTime(
                 sunrise
                     .overflowing_sub_signed(chrono::Duration::minutes(
@@ -67,17 +57,10 @@ impl Timestamps {
                     ))
                     .0,
             ),
-            optimal_sport_time: TimestampType::OptimalSportTime(
-                sunrise
-                    .overflowing_add_signed(chrono::Duration::hours(
-                        OPTIMAL_SPORTS_TIME_SINCE_SUNRISE_IN_HOURS,
-                    ))
-                    .0,
-            ),
-            optimal_evening_dinner_time: TimestampType::OptimalEveningDinnerTime(
+            dinner_time: TimestampType::DinnerTime(
                 sunrise
                     .overflowing_add_signed(chrono::Duration::seconds(
-                        (OPTIMAL_EVENING_DINNER_TIME_SINCE_SUNRISE_IN_HOURS * 3600.0) as i64,
+                        (DINNER_TIME_SINCE_SUNRISE_IN_HOURS * 3600.0) as i64,
                     ))
                     .0,
             ),
@@ -93,18 +76,11 @@ impl Timestamps {
     fn get_upcomming_timestamp(&self) -> &TimestampType {
         let now = Local::now().time();
         let mut upcomming_timestamp = &self.bed_time;
-        if let TimestampType::OptimalEveningDinnerTime(value) = self.optimal_evening_dinner_time {
+        if let TimestampType::DinnerTime(value) = self.dinner_time {
             if chrono::Duration::seconds(1).cmp(&now.signed_duration_since(value))
                 == Ordering::Greater
             {
-                upcomming_timestamp = &self.optimal_evening_dinner_time;
-            }
-        }
-        if let TimestampType::OptimalSportTime(value) = self.optimal_sport_time {
-            if chrono::Duration::seconds(1).cmp(&now.signed_duration_since(value))
-                == Ordering::Greater
-            {
-                upcomming_timestamp = &self.optimal_sport_time;
+                upcomming_timestamp = &self.dinner_time;
             }
         }
         if let TimestampType::WakeUpTime(value) = self.wake_up_time {
@@ -131,7 +107,7 @@ fn gather_input() -> NaiveTime {
                 .as_str(),
             "%H:%M",
         )
-        .expect("Wrong parameter. Expected time string.")
+        .expect("Wrong parameter. Expected %H:%M (9:47) as first arg.")
         .clamp(min_wakeup_time, max_wakeup_time)
     } else {
         panic!("Wrong time format. Expected %H:%M (9:47) as first arg.")
@@ -175,14 +151,25 @@ fn countdown_next_events(timestamps: Timestamps) {
 fn main() {
     let timestamps = Timestamps::new();
 
-    print!(" Wake up time:                    {} (-{}m)\n Optimal time to sport:           {} (+{}h)\n Optimal time for evening dinner: {} (+{}h)\n Bed time:                        {} (+{}h)\n",
-        if let TimestampType::WakeUpTime(value) = timestamps.wake_up_time { value.format("%H:%M") } else { format::DelayedFormat::new(None, None     , format::StrftimeItems::new("moin")) },
+    print!(
+        " Wake up time:   {} (-{}m)\n Evening dinner: {} (+{}h)\n Bed time:       {} (+{}h)\n",
+        if let TimestampType::WakeUpTime(value) = timestamps.wake_up_time {
+            value.format("%H:%M")
+        } else {
+            format::DelayedFormat::new(None, None, format::StrftimeItems::new("moin"))
+        },
         SUNRISE_MODIFIER_FOR_WAKE_UP_TIME_IN_MINUTES,
-        if let TimestampType::OptimalSportTime(value) = timestamps.optimal_sport_time { value.format("%H:%M") } else { format::DelayedFormat::new(None, None     , format::StrftimeItems::new("moin")) },
-        OPTIMAL_SPORTS_TIME_SINCE_SUNRISE_IN_HOURS,
-        if let TimestampType::OptimalEveningDinnerTime(value) = timestamps.optimal_evening_dinner_time { value.format("%H:%M") } else { format::DelayedFormat::new(None, None     , format::StrftimeItems::new("moin")) },
-        OPTIMAL_EVENING_DINNER_TIME_SINCE_SUNRISE_IN_HOURS,
-        if let TimestampType::BedTime(value) = timestamps.bed_time { value.format("%H:%M") } else { format::DelayedFormat::new(None, None     , format::StrftimeItems::new("moin")) },
+        if let TimestampType::DinnerTime(value) = timestamps.dinner_time {
+            value.format("%H:%M")
+        } else {
+            format::DelayedFormat::new(None, None, format::StrftimeItems::new("moin"))
+        },
+        DINNER_TIME_SINCE_SUNRISE_IN_HOURS,
+        if let TimestampType::BedTime(value) = timestamps.bed_time {
+            value.format("%H:%M")
+        } else {
+            format::DelayedFormat::new(None, None, format::StrftimeItems::new("moin"))
+        },
         SUNRISE_MODIFIER_FOR_BED_TIME_IN_HOURS
     );
 
